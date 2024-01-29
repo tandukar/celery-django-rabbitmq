2167ffc0dd4911478f2ef53f22f00626ed5114ea
Pragyo Tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-29 10:38:38 +0000

    configuring rabbitmq

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,19 +1,10 @@
+from __future__ import absolute_import, unicode_literals
 import os
-
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventorymanagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-
-app = Celery('proj')
+app = Celery('core')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
