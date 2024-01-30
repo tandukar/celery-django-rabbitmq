b5952965dd9d071a525b9a6de0aa13db99838018
Pragyo Tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-30 05:32:27 +0000

    creating reviewemailview

--- core/task2/views.py
+++ core/task2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from task2.forms import ReviewForm
+from django.http import HttpResponse
+from django.views.generic.edit import FormView
+
 
 # Create your views here.
+class ReviewEmailView(FormView):
+    template_name = 'review.html'
+    form_class = ReviewForm
+
+    def form_valid(self,form):
+        form.send_email()
+        return HttpResponse('Thanks for your review!')
