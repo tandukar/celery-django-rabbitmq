8a0f92ca6cac54ac5a984729182f4aed75773f76
Pragyo Tandukar <84461389+tandukar@users.noreply.github.com>
2024-01-30 07:58:47 +0000

    configuring email settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# gmail_send/settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tandukarpragyo@gmail.com'
+EMAIL_HOST_PASSWORD = 'jaazriuaslxfpwpg'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'tandukarpragyo@gmail.com'
